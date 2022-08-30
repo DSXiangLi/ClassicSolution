8e485c9afd1cc8e4e6031dfbc73d38ebacb1773b
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-08-30 08:17:22 +0800

    update min entropy

--- src/enhancement/min_entropy.py
+++ src/enhancement/min_entropy.py
@@ -1,15 +1,39 @@
 # -*-coding:utf-8 -*-
 import torch
+import torch.nn.functional as F
+
+
+class MinEnt(object):
+    def __init__(self, tp):
+        self.loss_fn = tp.loss_fn
+        self.alpha = tp.alpha
+        self.supervised_loss = 0
+        self.unsupervised_loss = 0
+
+    def compute_loss(self, features, logits):
+        labels = features['label']
+        cond = labels >= 0
+        # supervised_loss
+        self.supervised_loss = self.loss_fn(logits[cond], labels[cond])
+
+        # unsupervised entropy
+        cond = labels <0
+        probs = F.softmax(logits[cond], dim=-1)
+        self.unsupervised_loss = torch.sum(probs * torch.log(probs), dim=-1)
+        loss = self.supervised_loss + self.alpha * self.unsupervised_loss
+        return loss
 
 
 class PseudoLabel(object):
-    def __init__(self, tp, ):
+    def __init__(self, tp ):
         self.T1 = tp.T1
         self.T2 = tp.T2
         self.alpha_f = tp.alpha_f
         self.cur_epoch = 0
         self.loss_fn = tp.loss_fn
         self.alpha_t = 0
+        self.supervised_loss = 0
+        self.unsupervised_loss = 0
 
     def compute_loss(self, features, logits):
         labels = features['label']
