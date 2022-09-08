93e73c1821187d609b30acacc8bbcf77df9137ba
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-09-08 10:56:41 +0800

    update converter

--- src/dataset/converter.py
+++ src/dataset/converter.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 def single_text(text_list, label_list, data_dir, output_file):
     Fmt = namedtuple('SingleText', ['text1', 'label'])
 
-    with open(os.path.join(data_dir, output_file + '.txt'), 'w') as f:
+    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
         for t, l in zip(text_list, label_list):
             f.write(json.dumps(Fmt(t, l)._asdict(), ensure_ascii=False) + '\n')
 
@@ -20,7 +20,7 @@ def double_text(text_list1, text_list2, label_list, data_dir, output_file):
     """
     Fmt = namedtuple('SingleText', ['text1', 'text2', 'label'])
 
-    with open(os.path.join(data_dir, output_file + '.txt'), 'w') as f:
+    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
         for t1, t2, l in zip(text_list1, text_list2, label_list):
             f.write(json.dumps(Fmt(t1, t2, l)._asdict(), ensure_ascii=False) + '\n')
 
@@ -28,7 +28,7 @@ def double_text(text_list1, text_list2, label_list, data_dir, output_file):
 def single_text_double_label(text_list, label1_list, label2_list, data_dir, output_file):
     Fmt = namedtuple('SingleText', ['text1', 'label','label2'])
 
-    with open(os.path.join(data_dir, output_file + '.txt'), 'w') as f:
+    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='urf-8') as f:
         for t, l1, l2 in zip(text_list, label1_list, label2_list):
             f.write(json.dumps(Fmt(t, l1, l2)._asdict(), ensure_ascii=False) + '\n')
 
