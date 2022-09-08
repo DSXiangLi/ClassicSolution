dd1c695b81a2fc79e6008e58945af8af78ce9954
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-09-08 10:56:10 +0800

    add kaggle utils

---
+++ src/kaggle_utils.py
@@ -0,0 +1,19 @@
+# -*-coding:utf-8 -*-
+
+import os
+import zipfile
+
+
+def dir2zip(kaggle_path='/kaggle/working/', file_dir=''):
+    zip_path = os.path.join(kaggle_path, file_dir + '.zip')
+    zip = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+
+    for path, dirNames, fileNames in os.walk(os.path.join(kaggle_path, file_dir)):
+        fpath = path.replace(file_dir, '')
+        for name in fileNames:
+            # only save best checkpoint
+            if '.pth' in name and 'best' not in name:
+                continue
+            fullName = os.path.join(path, name)
+            name = fpath + '\\' + name
+            zip.write(fullName, name)
