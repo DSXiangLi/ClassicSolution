616067dbd96f6b7a8f3d8cfd13c35deb712ea1f0
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-09-09 21:09:37 +0800

    add new converter

--- src/dataset/converter.py
+++ src/dataset/converter.py
@@ -33,6 +33,18 @@ def single_text_double_label(id_list, text_list, label1_list, label2_list, data_
             f.write(json.dumps(Fmt(i, t, l1, l2)._asdict(), ensure_ascii=False) + '\n')
 
 
+def double_text_double_label(id_list, text_list1, text_list2, label1_list, label2_list, data_dir, output_file):
+    """
+    双文本输入Iterable
+    生成{'text1':'', 'text2':'', 'label':''}
+    """
+    Fmt = namedtuple('SingleText', ['id', 'text1', 'text2', 'label1', 'label2'])
+
+    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+        for i, t1, t2, l1, l2 in zip(id_list, text_list1, text_list2, label1_list, label2_list):
+            f.write(json.dumps(Fmt(i, t1, t2, l1, l2)._asdict(), ensure_ascii=False) + '\n')
+
+
 def json2df(file):
     lines = []
     with open(file, 'r') as f:
