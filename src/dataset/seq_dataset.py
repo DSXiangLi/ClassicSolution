543e62efdba1d080f64ae4273049bc6a1d66b3fe
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-09-20 20:38:19 +0800

    update seq dataset

--- src/dataset/seq_dataset.py
+++ src/dataset/seq_dataset.py
@@ -2,22 +2,28 @@
 import torch
 from torch.utils.data.dataset import Dataset
 
-
-class SeqDataset(Dataset):
-    def __init__(self, file_name, max_len, tokenizer, data_loader):
-        self.raw_data = data_loader(file_name)
-        self.max_len = max_len
+class SeqPairDataset(Dataset):
+    def __init__(self, data_loader, max_seq_len, tokenizer):
+        self.raw_data = data_loader()
+        self.max_seq_len = max_seq_len
         self.tokenizer = tokenizer
         self.features = []
         self.labels = []
         self.build_feature()
 
     def build_feature(self):
-        for data in self.raw_data:
-            self.features.append(self.tokenizer.encode_plus(data['text1'], padding='max_length',
-                                                            truncation=True, max_length=self.max_len))
-        if self.raw_data[0].get('label'):
-            self.labels = [i['label'] for i in self.raw_data]
+        if 'text2' in self.raw_data[0]:
+            for data in self.raw_data:
+                self.features.append(self.tokenizer.encode_plus(data['text1'], data['text2'], padding='max_length',
+                                                                truncation=True, max_length=self.max_seq_len,
+                                                                verbose=False))
+        else:
+            for data in self.raw_data:
+                self.features.append(self.tokenizer.encode_plus(data['text1'], padding='max_length',
+                                                                truncation=True, max_length=self.max_seq_len,
+                                                                verbose=False))
+        if 'label' in self.raw_data[0]:
+            self.labels = [data['label'] for data in self.raw_data]
 
     def __getitem__(self, idx):
         sample = self.features[idx]
@@ -27,7 +33,42 @@ class SeqDataset(Dataset):
         return sample
 
     def __len__(self):
-        return len(self.raw_data)
+        return len(self.features)
+
+
+class SeqPairMtlDataset(Dataset):
+    def __init__(self, data_loader, max_seq_len, tokenizer):
+        self.raw_data = data_loader()
+        self.max_seq_len = max_seq_len
+        self.tokenizer = tokenizer
+        self.features = []
+        self.labels = []
+        self.build_feature()
+
+    def build_feature(self):
+        if 'text2' in self.raw_data[0]:
+            for data in self.raw_data:
+                self.features.append(self.tokenizer.encode_plus(data['text1'], data['text2'], padding='max_length',
+                                                                truncation=True, max_length=self.max_seq_len,
+                                                                verbose=False))
+        else:
+            for data in self.raw_data:
+                self.features.append(self.tokenizer.encode_plus(data['text1'], padding='max_length',
+                                                                truncation=True, max_length=self.max_seq_len,
+                                                                verbose=False))
+        if 'label1' in self.raw_data[0]:
+            self.labels = [{'label1': data['label1'], 'label2': data['label2']} for data in self.raw_data]
+
+    def __getitem__(self, idx):
+        sample = self.features[idx]
+        sample = {k: torch.tensor(v) for k, v in sample.items()}
+        if self.labels:
+            sample['label1'] = self.labels[idx]['label1']
+            sample['label2'] = self.labels[idx]['label2']
+        return sample
+
+    def __len__(self):
+        return len(self.features)
 
 
 class SeqLabelDataset(Dataset):
