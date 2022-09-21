55035a05f68e38b2b709da6c74fcaa5f28cf2e41
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-09-21 09:53:40 +0800

    update seq label dataset for word split

--- src/dataset/seq_dataset.py
+++ src/dataset/seq_dataset.py
@@ -2,6 +2,7 @@
 import torch
 from torch.utils.data.dataset import Dataset
 
+
 class SeqPairDataset(Dataset):
     def __init__(self, data_loader, max_seq_len, tokenizer):
         self.raw_data = data_loader()
@@ -72,8 +73,8 @@ class SeqPairMtlDataset(Dataset):
 
 
 class SeqLabelDataset(Dataset):
-    def __init__(self, file_name, max_seq_len, tokenizer, data_loader, label2idx):
-        self.raw_data = data_loader(file_name)
+    def __init__(self, data_loader, tokenizer, max_seq_len, label2idx):
+        self.raw_data = data_loader()
         self.max_seq_len = max_seq_len
         self.tokenizer = tokenizer
         self.features = []
@@ -87,12 +88,13 @@ class SeqLabelDataset(Dataset):
             self.has_label = True
 
         for data in self.raw_data:
-            feature = self.tokenizer.encode_plus(data['text1'], padding='max_length',
+            feature = self.tokenizer.encode_plus(' '.join(data['text1'].split()), padding='max_length',
+                                                 is_split_into_words=True,  # split on white space
                                                  truncation=True, max_length=self.max_seq_len)
             self.features.append(feature)
             if self.has_label:
                 # set CLS, SEP, PAD to 'O' in label, so that they won't impact transition
-                label = data['label'][:(self.max_seq_len - 2)]
+                label = [self.label2idx[i] for i in data['label'][:(self.max_seq_len - 2)]]
                 label = [self.label2idx['O']] + label + [self.label2idx['O']]
                 label += [self.label2idx['O']] * (self.max_seq_len - len(label))
                 self.labels.append(label)
