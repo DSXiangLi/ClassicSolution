911bdc0900b37b68d06b2def94b1178b112491a0
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-09-27 16:03:25 +0800

    add span dataset

--- src/dataset/seq_dataset.py
+++ src/dataset/seq_dataset.py
@@ -73,6 +73,9 @@ class SeqPairMtlDataset(Dataset):
 
 
 class SeqLabelDataset():
+    """
+    Dataset for Sequence Labelling, by default only take single text input
+    """
     def __init__(self, data_loader, tokenizer, max_seq_len, label2idx):
         self.raw_data = data_loader()
         self.max_seq_len = max_seq_len
@@ -110,3 +113,44 @@ class SeqLabelDataset():
     def __len__(self):
         return len(self.raw_data)
 
+
+class SpanDataset():
+    def __init__(self, data_loader, tokenizer, max_seq_len):
+        self.raw_data = data_loader()
+        self.max_seq_len = max_seq_len
+        self.tokenizer = tokenizer
+        self.features = []
+        self.labels = []
+        self.build_feature()
+
+    def build_feature(self):
+        if self.raw_data[0].get('label'):
+            self.has_label = True
+
+        for data in self.raw_data:
+            feature = self.tokenizer.encode_plus(' '.join(data['text1']), padding='max_length',
+                                                 is_split_into_words=True,  # split on white space
+                                                 truncation=True, max_length=self.max_seq_len)
+            self.features.append(feature)
+            if self.has_label:
+                # set CLS, SEP, PAD to 'O' in label, so that they won't impact transition
+                label_start = data['label'][:(self.max_seq_len - 2)]
+                label_end = data['label2'][:(self.max_seq_len - 2)]
+                label_start = [0] + label_start + [0]
+                label_end = [0] + label_end + [0]
+                assert len(label_start) == sum(feature['attention_mask'])
+                assert len(label_end) == sum(feature['attention_mask'])
+                label_start += [0] * (self.max_seq_len - len(label_start))
+                label_end += [0] * (self.max_seq_len - len(label_end))
+                self.labels.append({'label_start': label_start, 'label_end':label_end})
+
+    def __getitem__(self, idx):
+        sample = self.features[idx]
+        sample = {k: torch.tensor(v) for k, v in sample.items()}
+        if self.labels:
+            sample['label_start'] = torch.tensor(self.labels[idx]['label_start'])
+            sample['label_end'] = torch.tensor(self.labels[idx]['label_end'])
+        return sample
+
+    def __len__(self):
+        return len(self.features)
