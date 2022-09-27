8b154ff706710584c6d99b7996ffb338ef606d08
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-09-27 16:56:13 +0800

    add bert_span model

--- fin_new_entity/models.py
+++ fin_new_entity/models.py
@@ -99,3 +99,74 @@ class BertCrf(nn.Module):
                                                     num_warmup_steps=self.tp.warmup_steps)
         return optimizer, scheduler
 
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from transformers import BertModel
+from src.loss import seqlabel_loss_wrapper
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+
+class BertSpan(nn.Module):
+    def __init__(self, tp):
+        super(BertSpan, self).__init__()
+        self.tp = tp
+        self.loss_fn = tp.loss_fn
+        self.label_size = tp.label_size
+        self.bert = BertModel.from_pretrained(tp.pretrain_model)
+        self.dropout_layer = nn.Dropout(tp.dropout_rate)
+        self.start_classifier = nn.Linear(self.bert.config.hidden_size, tp.label_size)
+        # end classifier : use start output and bert output
+        self.end_classifier = nn.Sequential(
+            nn.Linear(self.bert.config.hidden_size + tp.label_size, self.tp.hidden_size),
+            nn.Tanh(),
+            nn.LayerNorm(self.tp.hidden_size),
+            nn.Linear(self.tp.hidden_size, self.tp.label_size)
+        )
+
+    def forward(self, features):
+        """
+        features: {input_ids, token_type_ids, attention_mask, label_ids1, label_ids2}
+        """
+        outputs = self.bert(input_ids=features['input_ids'],
+                            token_type_ids=features['token_type_ids'],
+                            attention_mask=features['attention_mask'])
+        sequence_output = outputs[0]
+        sequence_output = self.dropout_layer(sequence_output)
+        logits_start = self.start_classifier(sequence_output)
+        start_pos = features.get('label_start')
+
+        if start_pos is not None and self.training:
+            # Training Stage: use input pos as soft label
+            batch_size, seq_len = start_pos.shape
+            start_soft_label = torch.zeros(batch_size, seq_len, self.tp.label_size, device=feature['input_ids'].device)
+            start_soft_label.scatter_(2, start_pos.unsqueeze(2), 1)
+        else:
+            # Inference Stage: use prediction as soft label
+            start_soft_label = F.softmax(logits_start, dim=-1)
+        logits_end = self.end_classifier(torch.concat([sequence_output, start_soft_label], dim=-1))
+        return logits_start, logits_end
+
+    def compute_loss(self, features, logits_pair):
+        loss1 = seqlabel_loss_wrapper(logits_pair[0], features['label_start'], features['attention_mask'], self.loss_fn)
+        loss2 = seqlabel_loss_wrapper(logits_pair[1], features['label_end'], features['attention_mask'], self.loss_fn)
+        loss = loss1 + loss2
+        return loss
+
+    def decode(self, features, logits_pair):
+        start_pred = torch.argmax(logits_pair[0], dim=-1)
+        end_pred = torch.argmax(logits_pair[1], dim=-1)
+        return start_pred, end_pred
+
+    def get_optimizer(self):
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        params = [
+            {'params': [p for n, p in self.named_parameters() if not any(nd in n for nd in no_decay)],
+             'weight_decay': self.tp.weight_decay},
+            {'params': [p for n, p in self.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+        optimizer = AdamW(params, lr=self.tp.lr, eps=self.tp.epsilon)
+        scheduler = get_linear_schedule_with_warmup(optimizer, num_training_steps=self.tp.num_train_steps,
+                                                    num_warmup_steps=self.tp.warmup_steps)
+        return optimizer, scheduler
