e21ba3fdf1cf7e97cd2a658388076e4f856808b6
DSXiangLi <37739462+DSXiangLi@users.noreply.github.com>
2022-10-15 21:16:00 +0800

    add None label for test datat

--- src/dataset/converter.py
+++ src/dataset/converter.py
@@ -16,11 +16,16 @@ def data_loader(file_name):
 
 
 def single_text(id_list, text_list, label_list, data_dir, output_file):
-    Fmt = namedtuple('SingleText', ['id', 'text1', 'label'])
-
-    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
-        for i, t, l in zip(id_list, text_list, label_list):
-            f.write(json.dumps(Fmt(i, t, l)._asdict(), ensure_ascii=False) + '\n')
+    if label_list is None:
+        Fmt = namedtuple('SingleText', ['id', 'text1'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t in zip(id_list, text_list):
+                f.write(json.dumps(Fmt(i, t)._asdict(), ensure_ascii=False) + '\n')
+    else:
+        Fmt = namedtuple('SingleText', ['id', 'text1', 'label'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t, l in zip(id_list, text_list, label_list):
+                f.write(json.dumps(Fmt(i, t, l)._asdict(), ensure_ascii=False) + '\n')
 
 
 def double_text(id_list, text_list1, text_list2, label_list, data_dir, output_file):
@@ -28,19 +33,30 @@ def double_text(id_list, text_list1, text_list2, label_list, data_dir, output_fi
     双文本输入Iterable
     生成{'text1':'', 'text2':'', 'label':''}
     """
-    Fmt = namedtuple('SingleText', ['id', 'text1', 'text2', 'label'])
 
-    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
-        for i, t1, t2, l in zip(id_list, text_list1, text_list2, label_list):
-            f.write(json.dumps(Fmt(i, t1, t2, l)._asdict(), ensure_ascii=False) + '\n')
+    if label_list is None:
+        Fmt = namedtuple('SingleText', ['id', 'text1', 'text2'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t1, t2 in zip(id_list, text_list1, text_list2):
+                f.write(json.dumps(Fmt(i, t1, t2)._asdict(), ensure_ascii=False) + '\n')
+    else:
+        Fmt = namedtuple('SingleText', ['id', 'text1', 'text2', 'label'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t1, t2, l in zip(id_list, text_list1, text_list2, label_list):
+                f.write(json.dumps(Fmt(i, t1, t2, l)._asdict(), ensure_ascii=False) + '\n')
 
 
 def single_text_double_label(id_list, text_list, label1_list, label2_list, data_dir, output_file):
-    Fmt = namedtuple('SingleText', ['id', 'text1', 'label', 'label2'])
-
-    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
-        for i, t, l1, l2 in zip(id_list, text_list, label1_list, label2_list):
-            f.write(json.dumps(Fmt(i, t, l1, l2)._asdict(), ensure_ascii=False) + '\n')
+    if label1_list is None:
+        Fmt = namedtuple('SingleText', ['id', 'text1'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t in zip(id_list, text_list):
+                f.write(json.dumps(Fmt(i, t)._asdict(), ensure_ascii=False) + '\n')
+    else:
+        Fmt = namedtuple('SingleText', ['id', 'text1', 'label', 'label2'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t, l1, l2 in zip(id_list, text_list, label1_list, label2_list):
+                f.write(json.dumps(Fmt(i, t, l1, l2)._asdict(), ensure_ascii=False) + '\n')
 
 
 def double_text_double_label(id_list, text_list1, text_list2, label1_list, label2_list, data_dir, output_file):
@@ -48,11 +64,16 @@ def double_text_double_label(id_list, text_list1, text_list2, label1_list, label
     双文本输入Iterable
     生成{'text1':'', 'text2':'', 'label':''}
     """
-    Fmt = namedtuple('SingleText', ['id', 'text1', 'text2', 'label1', 'label2'])
-
-    with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
-        for i, t1, t2, l1, l2 in zip(id_list, text_list1, text_list2, label1_list, label2_list):
-            f.write(json.dumps(Fmt(i, t1, t2, l1, l2)._asdict(), ensure_ascii=False) + '\n')
+    if label1_list is None:
+        Fmt = namedtuple('SingleText', ['id', 'text1', 'text2'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t1, t2 in zip(id_list, text_list1, text_list2):
+                f.write(json.dumps(Fmt(i, t1, t2)._asdict(), ensure_ascii=False) + '\n')
+    else:
+        Fmt = namedtuple('SingleText', ['id', 'text1', 'text2', 'label1', 'label2'])
+        with open(os.path.join(data_dir, output_file + '.txt'), 'w', encoding='utf-8') as f:
+            for i, t1, t2, l1, l2 in zip(id_list, text_list1, text_list2, label1_list, label2_list):
+                f.write(json.dumps(Fmt(i, t1, t2, l1, l2)._asdict(), ensure_ascii=False) + '\n')
 
 
 def json2df(file):
